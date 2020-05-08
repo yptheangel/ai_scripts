da41fcee0482254875cf08b639a14c170c7326ef
yptheangel <choowilson93@gmail.com>
2020-05-08 22:00:45 +0800

    added presplit train_test_split for object detection_pascal voc
    Signed-off-by: yptheangel <choowilson93@gmail.com>

---
+++ train_test_split_object_detection/split_train_test_od.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import numpy as np
+from shutil import copyfile
+import os.path as osp
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--folder", type=str, default="None")
+parser.add_argument("-t", "--train", type=int, default=80)
+args = parser.parse_args()
+print("Your dataset folder: {}".format(args.folder))
+print("Your train split percentage over full dataset: {}".format(args.train))
+
+dataset_folder = args.folder
+train = args.train
+
+# Why convert to set type? set does not allow duplicates, you can perform operations between 2 sets and it is faster than list
+# Set random seed if you wan reproduce results of your random sampling, but most of the time.. No
+
+# rng_seed = 000
+# np.random.seed(rng_seed)
+for root, dirs, files in os.walk(dataset_folder, topdown=False):
+    filelist=np.asarray(files)
+    train_split_num= int((train / 100) * len(filelist))
+    shuffled=np.random.choice(filelist,train_split_num,replace=False)
+    trainSplit=set(shuffled)
+    fullDataset=set(filelist)
+    testSplit = fullDataset.difference(trainSplit)
+    print("Label: {}, Total train images: {}, Total test images: {}".format(osp.basename(root), len(trainSplit), len(testSplit)))
+                    
+    trainSplit=list(trainSplit)
+    testSplit=list(testSplit)
+    
+    dst = osp.join("train")
+    os.makedirs(dst,exist_ok=False)
+    for img in trainSplit:          
+        copyfile(osp.join(root,img), osp.join(dst,img))
+        copyfile(osp.join(root,img[:-4]+".xml"), osp.join(dst,img[:-4]+".xml"))
+        
+    dst = osp.join("test")
+    os.makedirs(dst,exist_ok=False)
+    for img in testSplit:          
+        copyfile(osp.join(root,img), osp.join(dst,img))
+        copyfile(osp.join(root,img[:-4]+".xml"), osp.join(dst,img[:-4]+".xml"))
